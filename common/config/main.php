9c67d8d2c9ab31580d5b14e391fd70b5ef97d9c9
Jony <jony@qoo-app.com>
2020-12-15 14:22:28 +0800

    更新

--- common/config/main.php
+++ common/config/main.php
@@ -2,6 +2,10 @@
 return [
     'language' => 'zh-CN',
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm' => '@vendor/npm-asset',
+    ],
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
